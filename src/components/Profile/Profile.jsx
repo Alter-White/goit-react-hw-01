3e5bf338a848ff4564c058befa57a5b4945bc2ea
Ivan <ivanprime96@gmail.com>
2024-04-14 23:50:18 +0300

    added styles to task 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,31 @@
+import css from "./Profile.module.css";
+
 export default function Profile(props) {
   return (
-    <div>
-      <div>
-        <img src={props.image} alt="User avatar" width={100} />
-        <p>{props.name}</p>
-        <p>@{props.tag}</p>
-        <p>{props.location}</p>
+    <div className={css.container}>
+      <div className={css.pofileInfo}>
+        <img
+          className={css.image}
+          src={props.image}
+          alt="User avatar"
+          width={270}
+        />
+        <p className={css.userName}>{props.name}</p>
+        <p className={css.userText}>@{props.tag}</p>
+        <p className={css.userText}>{props.location}</p>
       </div>
-      <ul>
-        <li>
+      <ul className={css.profileList}>
+        <li className={css.profileStats}>
           <span>Followers</span>
-          <span>{props.stats.followers}</span>
+          <span className={css.profileValue}>{props.stats.followers}</span>
         </li>
-        <li>
+        <li className={css.profileStats}>
           <span>Views</span>
-          <span>{props.stats.views}</span>
+          <span className={css.profileValue}>{props.stats.views}</span>
         </li>
-        <li>
+        <li className={css.profileStats}>
           <span>Likes</span>
-          <span>{props.stats.likes}</span>
+          <span className={css.profileValue}>{props.stats.likes}</span>
         </li>
       </ul>
     </div>
