6a7aa277bfee1d29b33280cec3975ff72d148009
Ivan <ivanprime96@gmail.com>
2024-04-15 00:08:27 +0300

    added styles to second task

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,14 @@
+import css from "./FriendListItem.module.css";
+
 export default function FriendListItem({ friends }) {
   return (
-    <li key={friends.id}>
-      <div>
-        <img src={friends.avatar} alt="Avatar" width="48" />
-        <p>{friends.name}</p>
-        <p>{friends.isOnline ? "Online" : "Offline"}</p>
+    <li className={css.friendItem} key={friends.id}>
+      <div className={css.friendInfo}>
+        <img src={friends.avatar} alt="Avatar" width="200" />
+        <p className={css.friendName}>{friends.name}</p>
+        <p className={friends.isOnline ? css.online : css.offline}>
+          {friends.isOnline ? "Online" : "Offline"}
+        </p>
       </div>
     </li>
   );
