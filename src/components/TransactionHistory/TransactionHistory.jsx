4c7f133e43f0426f281eaa4f93202d7a03e640cb
Ivan <ivanprime96@gmail.com>
2024-04-15 00:23:44 +0300

    added styles to third task

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,21 +1,23 @@
+import css from "./TransactionHistory.module.css";
+
 export default function TransactionHistory(props) {
   return (
-    <table>
+    <table className={css.transactionTable}>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.tableHead}>Type</th>
+          <th className={css.tableHead}>Amount</th>
+          <th className={css.tableHead}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {props.items.map((element) => {
           return (
-            <tr key={element.id}>
-              <td>{element.type}</td>
-              <td>{element.amount}</td>
-              <td>{element.currency}</td>
+            <tr className={css.tableRows} key={element.id}>
+              <td className={css.tableItem}>{element.type}</td>
+              <td className={css.tableItem}>{element.amount}</td>
+              <td className={css.tableItem}>{element.currency}</td>
             </tr>
           );
         })}
